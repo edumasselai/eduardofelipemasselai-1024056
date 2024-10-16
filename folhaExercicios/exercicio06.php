7273623fbcd85417b0947ecf98baea8595df068b
edumasselai <eduardo.masselai@gmail.com>
2024-10-15 21:39:55 -0300

    ok

--- folhaExercicios/exercicio06.php
+++ folhaExercicios/exercicio06.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Feira do Joãozinho</title>
-    <link rel="exercicio06.css" href="styles.css">
+    <link rel="stylesheet" href="exercicio06.css">
 </head>
 <body>
 
